a611b4dca2b2336e9a94a20dec13711a924ae5fa
ant6009 <ant.rivoire@gmail.com>
2025-05-03 13:57:24 +0100

    cleaning repo

--- modules/nixos/services/tftpd/default.nix
+++ modules/nixos/services/tftpd/default.nix
@@ -1,11 +1,11 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  services.tfptd = {
-    enable = true;
-    path = "/srv/tftp";
-    port = 69;
+{pkgs, ...}: {
+  services.atftpd = {
+    enable = false;
+    root = "/srv/tftp";
+    extraOptions = [
+      "--bind-address 192.168.0.66"
+      "--verbose=7"
+      "--port=69"
+    ];
   };
 }
