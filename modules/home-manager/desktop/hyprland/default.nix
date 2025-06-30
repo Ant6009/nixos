05566ffdcdd7fc46ee6a6a4493a4e2dea471776c
ant6009 <ant.rivoire@gmail.com>
2025-06-30 15:00:13 +0100

    hypridle functional

--- modules/home-manager/desktop/hyprland/default.nix
+++ modules/home-manager/desktop/hyprland/default.nix
@@ -17,6 +17,7 @@
     "${nhModules}/services/waybar"
     "${nhModules}/services/wlogout"
     "${nhModules}/services/hyprpaper"
+    "${nhModules}/services/hyprlock"
   ];
 
   # Consistent cursor theme across all applications.
@@ -49,156 +50,163 @@
         before_sleep_cmd = loginctl lock-session
              after_sleep_cmd = hyprctl dispatch dpms on
            }
-           listener {
-           on-resumebrightnessctl -rd rgb:kbd_backlight
-       on-timeout=brightnessctl -sd rgb:kbd_backlight set 0
-        timeout=300
+      #              listener {
+      #              on-resumebrightnessctl -rd rgb:kbd_backlight
+      #          on-timeout=brightnessctl -sd rgb:kbd_backlight set 0
+      #           timeout=300
+      #         }
+
+      listener {
+        timeout = 60
+        on-timeout = loginctl lock-session && brightnessctl -s set 10
+        on-resume = notify-send "Resuming" && brightnessctl -r
       }
 
       listener {
-        on-timeout=loginctl lock-session
-        timeout=360
-      }
-
-      listener {
-        on-resume=hyprctl dispatch dpms on
         on-timeout=hyprctl dispatch dpms off
-        timeout=420
+        on-resume=hyprctl dispatch dpms on
+
+        timeout=450
+      }
+
+      listener {
+        on-timeout= systemctl suspend
+        timeout=1000
       }
       }
     '';
 
-    "hypr/hyprlock.conf".text = ''
-      background {
-          monitor = HDMI-A-1
-          path = ${config.wallpaper}
-          blur_passes = 3
-          contrast = 0.8916
-          brightness = 0.8172
-          vibrancy = 0.1696
-          vibrancy_darkness = 0.0
-      }
+    #   "hypr/hyprlock.conf".text = ''
+    #     background {
+    #         monitor = HDMI-A-1
+    #         path = ${config.wallpaper}
+    #         blur_passes = 3
+    #         contrast = 0.8916
+    #         brightness = 0.8172
+    #         vibrancy = 0.1696
+    #         vibrancy_darkness = 0.0
+    #     }
 
-      general {
-          no_fade_in = false
-          grace = 0
-          disable_loading_bar = true
-      }
+    #     general {
+    #         no_fade_in = false
+    #         grace = 0
+    #         disable_loading_bar = true
+    #     }
 
-      # HDMI-A-1  Conifg
-      input-field {
-          monitor = HDMI-A-1
-          size = 250, 60
-          outline_thickness = 2
-          dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
-          dots_center = true
-          outer_color = rgba(0, 0, 0, 0)
-          inner_color = rgba(0, 0, 0, 0.5)
-          font_color = rgb(200, 200, 200)
-          fade_on_empty = false
-          capslock_color = -1
-          placeholder_text = <i><span foreground="##e6e9ef">Password</span></i>
-          fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
-          hide_input = false
-          position = 0, -120
-          halign = center
-          valign = center
-      }
+    #     # HDMI-A-1  Conifg
+    #     input-field {
+    #         monitor = HDMI-A-1
+    #         size = 250, 60
+    #         outline_thickness = 2
+    #         dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
+    #         dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
+    #         dots_center = true
+    #         outer_color = rgba(0, 0, 0, 0)
+    #         inner_color = rgba(0, 0, 0, 0.5)
+    #         font_color = rgb(200, 200, 200)
+    #         fade_on_empty = false
+    #         capslock_color = -1
+    #         placeholder_text = <i><span foreground="##e6e9ef">Password</span></i>
+    #         fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
+    #         hide_input = false
+    #         position = 0, -120
+    #         halign = center
+    #         valign = center
+    #     }
 
-      # Date
-      label {
-        monitor = HDMI-A-1
-        text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
-        color = rgba(255, 255, 255, 0.8)
-        font_size = 15
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, -400
-        halign = center
-        valign = top
-      }
+    #     # Date
+    #     label {
+    #       monitor = HDMI-A-1
+    #       text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
+    #       color = rgba(255, 255, 255, 0.8)
+    #       font_size = 15
+    #       font_family = JetBrains Mono Nerd Font Mono ExtraBold
+    #       position = 0, -400
+    #       halign = center
+    #       valign = top
+    #     }
 
-      # Time
-      label {
-          monitor = HDMI-A-1
-          text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
-          color = rgba(255, 255, 255, 0.8)
-          font_size = 120
-          font_family = JetBrains Mono Nerd Font Mono ExtraBold
-          position = 0, -400
-          halign = center
-          valign = top
-      }
+    #     # Time
+    #     label {
+    #         monitor = HDMI-A-1
+    #         text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
+    #         color = rgba(255, 255, 255, 0.8)
+    #         font_size = 120
+    #         font_family = JetBrains Mono Nerd Font Mono ExtraBold
+    #         position = 0, -400
+    #         halign = center
+    #         valign = top
+    #     }
 
-      # Keyboard layout
-      label {
-        monitor = HDMI-A-1
-        text = $LAYOUT
-        color = rgba(255, 255, 255, 0.9)
-        font_size = 10
-        font_family = JetBrains Mono Nerd Font Mono
-        position = 0, -175
-        halign = center
-        valign = center
-      }
+    #     # Keyboard layout
+    #     label {
+    #       monitor = HDMI-A-1
+    #       text = $LAYOUT
+    #       color = rgba(255, 255, 255, 0.9)
+    #       font_size = 10
+    #       font_family = JetBrains Mono Nerd Font Mono
+    #       position = 0, -175
+    #       halign = center
+    #       valign = center
+    #     }
 
-      # eHDMI-A-1  Conifg
-      input-field {
-          monitor = eHDMI-A-1
-          size = 500, 120
-          outline_thickness = 2
-          dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
-          dots_center = true
-          outer_color = rgba(0, 0, 0, 0)
-          inner_color = rgba(0, 0, 0, 0.5)
-          font_color = rgb(200, 200, 200)
-          fade_on_empty = false
-          capslock_color = -1
-          placeholder_text = <i><span foreground="##e6e9ef">Password</span></i>
-          fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
-          hide_input = false
-          position = 0, -120
-          halign = center
-          valign = center
-      }
+    #     # eHDMI-A-1  Conifg
+    #     input-field {
+    #         monitor = eHDMI-A-1
+    #         size = 500, 120
+    #         outline_thickness = 2
+    #         dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
+    #         dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
+    #         dots_center = true
+    #         outer_color = rgba(0, 0, 0, 0)
+    #         inner_color = rgba(0, 0, 0, 0.5)
+    #         font_color = rgb(200, 200, 200)
+    #         fade_on_empty = false
+    #         capslock_color = -1
+    #         placeholder_text = <i><span foreground="##e6e9ef">Password</span></i>
+    #         fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
+    #         hide_input = false
+    #         position = 0, -120
+    #         halign = center
+    #         valign = center
+    #     }
 
-      # Date
-      label {
-        monitor = eHDMI-A-1
-        text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
-        color = rgba(255, 255, 255, 0.8)
-        font_size = 30
-        font_family = JetBrains Mono Nerd Font Mono ExtraBold
-        position = 0, -400
-        halign = center
-        valign = top
-      }
+    #     # Date
+    #     label {
+    #       monitor = eHDMI-A-1
+    #       text = cmd[update:1000] echo "<span>$(date '+%A, %d %B')</span>"
+    #       color = rgba(255, 255, 255, 0.8)
+    #       font_size = 30
+    #       font_family = JetBrains Mono Nerd Font Mono ExtraBold
+    #       position = 0, -400
+    #       halign = center
+    #       valign = top
+    #     }
 
-      # Time
-      label {
-          monitor = eHDMI-A-1
-          text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
-          color = rgba(255, 255, 255, 0.8)
-          font_size = 240
-          font_family = JetBrains Mono Nerd Font Mono ExtraBold
-          position = 0, -400
-          halign = center
-          valign = top
-      }
+    #     # Time
+    #     label {
+    #         monitor = eHDMI-A-1
+    #         text = cmd[update:1000] echo "<span>$(date '+%H:%M')</span>"
+    #         color = rgba(255, 255, 255, 0.8)
+    #         font_size = 240
+    #         font_family = JetBrains Mono Nerd Font Mono ExtraBold
+    #         position = 0, -400
+    #         halign = center
+    #         valign = top
+    #     }
 
-      # Keyboard layout
-      label {
-        monitor = eHDMI-A-1
-        text = $LAYOUT
-        color = rgba(255, 255, 255, 0.9)
-        font_size = 20
-        font_family = JetBrains Mono Nerd Font Mono
-        position = 0, -230
-        halign = center
-        valign = center
-      }
-    '';
+    #     # Keyboard layout
+    #     label {
+    #       monitor = eHDMI-A-1
+    #       text = $LAYOUT
+    #       color = rgba(255, 255, 255, 0.9)
+    #       font_size = 20
+    #       font_family = JetBrains Mono Nerd Font Mono
+    #       position = 0, -230
+    #       halign = center
+    #       valign = center
+    #     }
+    #   '';
   };
 
   dconf.settings = {
