28be6c2f4d63630438973450d6dc56b83d48cfb2
ant6009 <ant.rivoire@gmail.com>
2025-10-27 19:46:38 +0000

    added config for new machine

---
+++ home/antoine/razorback/default.nix
@@ -0,0 +1,21 @@
+{nhModules, ...}: {
+  imports = [
+    "${nhModules}/common"
+    "${nhModules}/desktop/hyprland"
+    "${nhModules}/services/easyeffects"
+    "${nhModules}/services/ulauncher"
+    "${nhModules}/programs/nvf"
+    "${nhModules}/services/hyprpaper"
+    "${nhModules}/services/hyprlock"
+    "${nhModules}/misc/wallpaper"
+  ];
+
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  # home.stateVersion = "24.11";
+}
