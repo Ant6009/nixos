3edae8e37460beeb4be50295d7a95528489f7a7a
Antoine Rivoire <ant.rivoire@gmail.com>
2025-11-28 12:17:29 +0000

    latest darwin changes

---
+++ modules/nixos/music/default.nix
@@ -0,0 +1,213 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  userConfig,
+  pkgs,
+  ...
+}: {
+  # Nixpkgs configuration
+  nixpkgs = {
+    overlays = [
+  #    outputs.overlays.stable-packages
+    ];
+
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  # Register flake inputs for nix commands
+  nix.registry = lib.mapAttrs (_: flake: {inherit flake;}) (lib.filterAttrs (_: lib.isType "flake") inputs);
+
+  # Musnix configuration
+  musnix.enable = true;
+  musnix.kernel.realtime = true;
+  musnix.rtirq.enable = true;
+
+
+  # Nix settings
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    auto-optimise-store = true;
+    trusted-users = ["root" "antoine" "a.rivoire"];
+  };
+
+  # Boot settings
+  boot = {
+    kernelParams = ["quiet" "splash"];
+    loader.efi.canTouchEfiVariables = true;
+    loader.systemd-boot.enable = true;
+
+    # v4l (virtual camera) module settings
+    kernelModules = ["v4l2loopback"];
+    extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    '';
+  };
+
+  # Networking
+  networking.networkmanager.enable = true;
+
+  # Timezone
+  time.timeZone = "Europe/Dublin";
+
+  # Internationalization
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IE.UTF-8";
+    LC_IDENTIFICATION = "en_IE.UTF-8";
+    LC_MEASUREMENT = "en_IE.UTF-8";
+    LC_MONETARY = "en_IE.UTF-8";
+    LC_NAME = "en_IE.UTF-8";
+    LC_NUMERIC = "en_IE.UTF-8";
+    LC_PAPER = "en_IE.UTF-8";
+    LC_TELEPHONE = "en_IE.UTF-8";
+    LC_TIME = "en_IE.UTF-8";
+  };
+
+  # Input settings
+  services.libinput.enable = true;
+
+  # X11 settings
+  services.xserver = {
+    enable = true;
+    xkb.layout = "gb";
+    xkb.variant = "mac_intl";
+    excludePackages = with pkgs; [xterm];
+    displayManager.gdm.enable = true;
+  };
+
+  # PATH configuration
+  environment.localBinInPath = true;
+
+  # Disable CUPS printing
+  services.printing.enable = false;
+
+  # Enable devmon for device management
+  services.devmon.enable = true;
+
+  # Enable PipeWire for sound
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # automount/unmount drives
+  services.udisks2.enable = true;
+  services.gvfs.enable = true;
+
+  # User configuration
+  users.users.${userConfig.name} = {
+    description = userConfig.fullName;
+    extraGroups = ["audio" "plugdev" "networkmanager" "wheel" "camera" "dialout" "libvirtd" "storage"];
+    isNormalUser = true;
+    shell = pkgs.zsh;
+  };
+
+  # Set User's avatar
+  system.activationScripts.script.text = ''
+    mkdir -p /var/lib/AccountsService/{icons,users}
+    cp ${userConfig.avatar} /var/lib/AccountsService/icons/${userConfig.name}
+
+    touch /var/lib/AccountsService/users/${userConfig.name}
+
+    if ! grep -q "^Icon=" /var/lib/AccountsService/users/${userConfig.name}; then
+      if ! grep -q "^\[User\]" /var/lib/AccountsService/users/${userConfig.name}; then
+        echo "[User]" >> /var/lib/AccountsService/users/${userConfig.name}
+      fi
+      echo "Icon=/var/lib/AccountsService/icons/${userConfig.name}" >> /var/lib/AccountsService/users/${userConfig.name}
+    fi
+  '';
+
+  # Passwordless sudo
+  security.sudo.wheelNeedsPassword = false;
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    gcc
+    glib
+    gnumake
+    killall
+    mesa
+    unzip
+    wget
+    curl
+    kitty
+    firefox
+    vim
+    home-manager
+    quickemu
+    shotcut
+    ardour
+    cardinal
+    nmap
+    tcpdump
+    zoxide
+    sox
+    bc
+    qjackctl
+    alsa-utils
+    ffmpeg
+  ];
+
+  # Docker configuration
+  # virtualisation.docker.enable = true;
+  #virtualisation.docker.rootless.enable = true;
+  #virtualisation.docker.rootless.setSocketVariable = true;
+  # virtualisation.libvirtd = {
+  #   enable = true;
+  #   qemu = {
+  #     package = pkgs.qemu_kvm;
+  #     runAsRoot = true;
+  #     swtpm.enable = true;
+  #     ovmf = {
+  #       enable = true;
+  #       packages = [
+  #         (pkgs.OVMF.override {
+  #           secureBoot = true;
+  #           tpmSupport = true;
+  #         }).fd
+  #       ];
+  #     };
+  #   };
+  # };
+   #  virtualisation.spiceUSBRedirection.enable = true;
+   # virtualisation.libvirtd.enable = true;
+
+  # Zsh configuration
+  programs.zsh.enable = true;
+
+  # Fonts configuration
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.meslo-lg
+    roboto
+  ];
+
+  # Additional services
+  services.locate.enable = true;
+
+  # OpenSSH daemon
+  services.openssh.enable = true;
+
+  # Let devenv manage caches in the nix store
+
+users.groups.plugdev = {};
+
+services.udev.extraRules = ''
+  # Teensy boards for M8
+  ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+  SUBSYSTEM=="hidraw", ATTRS{idVendor}=="16c0", MODE="0666"
+  SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", MODE:="0666"
+'';
+}
